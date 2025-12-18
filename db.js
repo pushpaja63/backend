11c25bacab4c93a161c8ee921cb5191e9c23d525
pushpaja63 <pushpaja.allamsetty@gmail.com>
2025-12-18 15:51:04 +0530

    Backend ready for Render

--- db.js
+++ db.js
@@ -10,8 +10,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Nokia@1234',  // change if you use another password
-  database: process.env.DB_NAME || 'myappdb',      // must match the DB you created
+  password: process.env.DB_PASSWORD || 'root',  // change if you use another password
+  database: process.env.DB_NAME || 'testdb',      // must match the DB you created
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
